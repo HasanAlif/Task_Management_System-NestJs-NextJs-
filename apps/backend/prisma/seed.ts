57569c7fa091904829881eb9841b0828852d5148
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-30 00:42:37 +0600

    Seeding Prisma

---
+++ apps/backend/prisma/seed.ts
@@ -0,0 +1,58 @@
+import { fa, faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+function generateSlug(title: string): string {
+  return title
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-|-$/g, '');
+}
+
+async function main() {
+  const users = Array.from({ length: 10 }).map(() => ({
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    bio: faker.lorem.sentence(),
+    avatar: faker.image.avatar(),
+  }));
+
+  await prisma.user.createMany({
+    data: users,
+  });
+
+  const tasks = Array.from({ length: 40 }).map(() => ({
+    title: faker.lorem.sentence(),
+    slug: generateSlug(faker.lorem.sentence()),
+    content: faker.lorem.paragraphs(3),
+    thumbnail: faker.image.urlLoremFlickr(),
+    authorId: faker.number.int({ min: 1, max: 10 }),
+    published: true,
+  }));
+
+  await Promise.all(
+    tasks.map(
+      async (task) =>
+        await prisma.task.create({
+          data: {
+            ...task,
+          },
+        }),
+    ),
+  );
+
+  console.log('Seeding completed successfully!');
+}
+
+main()
+  .then(() => {
+    prisma.$disconnect();
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error('Error during seeding:', error);
+    prisma.$disconnect();
+    process.exit(1);
+  });
